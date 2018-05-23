868f232f17cd7d9206b54ac469270e6d4ded0003
Florian Goessler <florian.goessler@scout24.com>
2018-05-23 08:54:33 +0200

    improve formatting for expired scloud AWS login

--- powerline_shell/segments/scloud.py
+++ powerline_shell/segments/scloud.py
@@ -9,7 +9,10 @@ class Segment(BasicSegment):
             now = datetime.datetime.now();
             exp = datetime.datetime.strptime(expDate, "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(hours=2)
             diff = exp - now
-            scloud_state = ':'.join(str(diff).split(':')[:2])
-            self.powerline.append(" AWS %s " % scloud_state, 15, 2 if diff.seconds > 0 else 1)
+            if diff.total_seconds() > 0:
+                scloud_state = ':'.join(str(diff).split(':')[:2])
+                self.powerline.append(" AWS %s " % scloud_state, 15, 2)
+            else:
+                self.powerline.append(" AWS *EXP* ", 15, 1)
         else:
             self.powerline.append(" AWS - ", 15, 1)
